5d5cdda617919adbc9f87a57260d51d8ec64f618
phuc.nguyen <hphuc.np@gmail.com>
2026-03-10 14:09:31 +0700

    Fix tests

--- src/test/java/com/example/emergencyapi/EmergencyControllerTest.java
+++ src/test/java/com/example/emergencyapi/EmergencyControllerTest.java
@@ -11,9 +11,9 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.example.emergencyapi.controller.GlobalExceptionHandler;
@@ -30,7 +30,7 @@ class EmergencyControllerTest {
         @Autowired
         private MockMvc mockMvc;
 
-        @MockBean
+        @MockitoBean
         private EmergencyService emergencyService;
 
         @Test
